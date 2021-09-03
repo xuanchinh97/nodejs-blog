c2ab7066350a4da3c99629d90280cd308338993c
chinhdz97 <kqtchcheng97@gmail.com>
2021-09-03 15:27:56 +0700

    add lib: express, morgan, nodemon, handlebars

---
+++ scr/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const morgan = require('morgan')
+const exphbs  = require('express-handlebars');
+const path = require('path')
+
+const app = express()
+//sử dụng margo để log
+app.use(morgan('combined'))
+
+//sử dụng express-handlebars
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+//set url source file
+app.set('views', path.join(__dirname,'sources/views'))
+console.log(path.join(__dirname,'sources/views'));
+//tên trang trangchu và render ra home
+app.get('/trang-chu', function (req, res) {
+    res.render('home');
+});
+//tên trang tintuc và render ra news
+app.get('/tin-tuc', function (req, res) {
+    res.render('news');
+});
+
+app.listen(3000)
