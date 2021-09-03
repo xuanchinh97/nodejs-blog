8460e37586f6e436313f53a978553f2b352d1871
chinhdz97 <kqtchcheng97@gmail.com>
2021-09-03 19:44:35 +0700

    add bootstrap and lib SASS

--- scr/index.js
+++ scr/index.js
@@ -1,22 +1,30 @@
 const express = require('express')
 const morgan = require('morgan')
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const path = require('path')
 
+
+
 const app = express()
+
+//khai báo url tĩnh
+app.use(express.static(path.join(__dirname, 'public')))
+
 //sử dụng margo để log
 app.use(morgan('combined'))
 
 //sử dụng express-handlebars
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
 //set url source file
-app.set('views', path.join(__dirname,'sources/views'))
-console.log(path.join(__dirname,'sources/views'));
+app.set('views', path.join(__dirname, 'sources/views'))
+
 //tên trang trangchu và render ra home
 app.get('/trang-chu', function (req, res) {
     res.render('home');
 });
+
 //tên trang tintuc và render ra news
 app.get('/tin-tuc', function (req, res) {
     res.render('news');
