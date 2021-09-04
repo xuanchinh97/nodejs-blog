3cf078673856fc8e96d2459008c67126eff8cc1e
chinhdz97 <kqtchcheng97@gmail.com>
2021-09-04 23:23:35 +0700

    hook

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const route = require('./routes');
 
 const app = express();
 
-app.use(express.json());
+        app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //khai báo url tĩnh
