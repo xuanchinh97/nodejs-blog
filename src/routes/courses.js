966f8ed321a44f233b60e5f86fbfb33bfe33be95
chinhdz97 <kqtchcheng97@gmail.com>
2021-09-08 21:51:51 +0700

    select all with checkbox 32

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,11 +5,12 @@ const courseController = require('../app/controllers/CourseController');
 
 router.get('/create', courseController.create);
 router.post('/store', courseController.store);
+router.post('/handle-form-actions', courseController.handleFormActions);
 router.get('/:id/edit', courseController.edit);
 router.patch('/:id/restore', courseController.restore);
 router.put('/:id', courseController.update);
 router.delete('/:id/destroy', courseController.destroy);
 router.delete('/:id', courseController.delete);
-router.get('/:slug', courseController.show);
+// router.get('/:slug', courseController.show);
 
 module.exports = router;
